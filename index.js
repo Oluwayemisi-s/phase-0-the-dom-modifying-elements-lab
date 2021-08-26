6add08732022ab1791cd80ead09e1631619833eb
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-08-26 12:46:09 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.getElementById('main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "<h1> Oluwayemisi is the champion! </h1>";
