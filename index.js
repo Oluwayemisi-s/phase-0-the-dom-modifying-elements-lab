74642eaf4451f8ea9e98159093186bfa8127c48f
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-09-26 23:23:12 -0700

    commit tree 16d58be9b787b5f6db3d492c476899fad0743b1b

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 // Write your code here!
-const main = document.getElementById('main');
 
-main.remove();
+const main = document.getElementById('main')
+main.remove()
 
-const newHeader = document.createElement('h1');
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Oluwayemisi is the champion!!!'
 
-document.body.appendChild(newHeader);
+document.body.appendChild(newHeader)
 
-newHeader.id = 'victory';
 
-newHeader.innerHTML = "<h1> Oluwayemisi is the champion! </h1>";
+
+
+
+
+
+
+
+
+
+// const main = document.getElementById('main');
+
+// main.remove();
+
+// const newHeader = document.createElement('h1');
+
+// document.body.appendChild(newHeader);
+
+// newHeader.id = 'victory';
+
+// newHeader.innerHTML = "<h1> Oluwayemisi is the champion! </h1>";
